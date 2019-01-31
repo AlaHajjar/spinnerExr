b7359ddf4892955c8bb34cddb0760b96f875bf6f
AlaHajjar <hjarly426@gmail.com>
2019-01-31 21:02:16 +0300

    Replace spinner with dialog

--- app/src/main/java/com/example/android/languageperfersnce/WelcomeAct.java
+++ app/src/main/java/com/example/android/languageperfersnce/WelcomeAct.java
@@ -1,10 +1,15 @@
 package com.example.android.languageperfersnce;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -15,87 +20,118 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 
-public class WelcomeAct extends AppCompatActivity {
-Spinner langSpinner;
+public class WelcomeAct extends AppCompatActivity  {
+    //Spinner langSpinner;
+    TextView mText;
+    Locale mlocale;
+    Button btn;
 
-Locale mlocale;
-Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-       // setLocale(Locale.getDefault().getLanguage());
         loadLocale();
+        setContentView(R.layout.activity_welcome);
 
-        langSpinner = (Spinner)findViewById(R.id.spn1);
-        btn = (Button)findViewById(R.id.btn);
-        //languageNametxt = (TextView) findViewById(R.id.language);
 
-        setupSpinner();
+
+
+      //  langSpinner = (Spinner) findViewById(R.id.spn1);
+        mText = (TextView)findViewById(R.id.tv);
+
+
+        btn = (Button) findViewById(R.id.btn);
+       // setupSpinner();
+        String lang = Locale.getDefault().getLanguage();
+        if (lang.equals("en")){
+            mText.setText("English");
+        }else if (lang.equals("ar")){
+            mText.setText("العربية ");
+        }
+
+
     }
 
-    private void setupSpinner(){
-        ArrayAdapter spinAdapter = ArrayAdapter.createFromResource(this,R.array.languages_array,android.R.layout.simple_spinner_dropdown_item);
-       spinAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-       langSpinner.setAdapter(spinAdapter);
-       langSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-           @Override
-           public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-              String s = (String)adapterView.getItemAtPosition(i);
-
-               if (s.equals(getString(R.string.arabic)) && btn.getText().equals("Start") ){
-              // if(!(Locale.getDefault().getLanguage().equals("ar"))){
-                  setLocale("ar");
-                  recreate();
-              }else  if (s.equals(getString(R.string.english)) && btn.getText().equals("دخول")){
-                 // if(!(Locale.getDefault().getLanguage().equals("en"))){
-                   setLocale("en");
-
-                   recreate();}
-           }
 
 
-           @Override
-           public void onNothingSelected(AdapterView<?> adapterView) {
-               // Another interface call back
-           }
-       });
+
+
+
+
+
+
+
+
+    public void loadLocale() {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs",
+                Activity.MODE_PRIVATE);
+        String language = prefs.getString(langPref, "ar");
+        changeLang(language);
     }
 
-    public void setLocale(String language) {
-         mlocale = new Locale(language);
+    public void changeLang(String lang) {
+        if (lang.equalsIgnoreCase(""))
+            return;
+        mlocale = new Locale(lang);
         Locale.setDefault(mlocale);
-        Resources res = this.getResources();
+        saveLocale(lang);
 
-        Configuration confi = res.getConfiguration();
-        confi.locale = mlocale;
-        res.updateConfiguration(confi,res.getDisplayMetrics());
+        Configuration config = new Configuration();
+        config.setLocale(mlocale);
+        getBaseContext().getResources().updateConfiguration(config,getBaseContext().getResources().getDisplayMetrics());
 
+    }
 
-       SharedPreferences.Editor  editor = getSharedPreferences("lang",MODE_PRIVATE).edit();
-        editor.putString("my Language",language);
-
+    public void saveLocale(String lang) {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs",
+                Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(langPref, lang);
         editor.apply();
-
-
     }
 
-    public void loadLocale()
-    {
-        SharedPreferences pref = getSharedPreferences("lang", Activity.MODE_PRIVATE);
-        String language = pref.getString("my Language","");
+        public void Go_btn (View view){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
 
-        setLocale(language);
+        }
 
+
+    public void choseLanguage(View view) {
+        builtAlertDialog();
     }
 
-    public void Go_btn(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-        finish();
+    public void builtAlertDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        int checkedItem;
+        String systemLang =Locale.getDefault().getLanguage();
+         if (systemLang.equals("en")){checkedItem=0;}else
+             if (systemLang.equals("ar")){checkedItem=1;}else checkedItem=0;
+
+
+        dialogBuilder.setTitle("Chose Language..").setSingleChoiceItems(R.array.languages_array, checkedItem, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i==0){
+                    changeLang("en");
+                    recreate();
+                }else if (i==1 && !Locale.getDefault().getLanguage().equals("ar")){
+                    changeLang("ar");
+                    recreate();
+                }
+                dialogInterface.dismiss();
+            }
+
+        });
+       AlertDialog dialog= dialogBuilder.create();
+       dialog.show();
     }
 }
